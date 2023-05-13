fb94f65c2c255cd123813b1e197fc04122814d85
Pedro Dos Santos <116087473+LordPepas@users.noreply.github.com>
2023-05-13 19:18:08 -0300

    Arquivos-WDA

---
+++ WDA-Livraria(Web)/public_html/WDA-Livraria/CREATE/C_usuario.php
@@ -0,0 +1,95 @@
+<head>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>;
+</head>
+<?php
+if (isset($_POST['submit'])) { // verifica se o formulário foi submetido
+    include_once('../Config.php'); // inclui o arquivo de configuração do banco de dados
+
+    $nome = $_POST['nome'];
+    $cidade = $_POST['cidade'];
+    $endereço = $_POST['endereço'];
+    $email = $_POST['email'];
+
+    $sqlcliente = "SELECT * FROM usuario WHERE Nome_usuario ='$nome'"; // consulta para verificar se o usuário já existe
+
+    $resultado = $conexao->query($sqlcliente); // executa a consulta
+    mysqli_query($conexao, "ALTER TABLE id20742095_bdlocadora.usuario AUTO_INCREMENT = 1;");
+
+    if (mysqli_num_rows($resultado) == 1) { // verifica se encontrou algum registro
+        $mensagem = 'Usuário já existe'; // define a mensagem de erro
+    } else {
+        $result = mysqli_query($conexao, "INSERT INTO usuario (Nome_usuario,Cidade,Endereço,Email)
+        VALUES ('$nome','$cidade','$endereço','$email')"); // insere o novo usuário no banco de dados
+        if (!$result) { // verifica se ocorreu algum erro ao inserir o registro
+            $mensagem = "Erro ao cadastrar usuário: " . mysqli_error($conexao); // define a mensagem de erro
+        } else {
+            echo "<script type='text/javascript'>Swal.fire('Excelente!','Cliente adicionado com sucesso!','success').then(() => {window.location.href = '../SISTEMA/Usuarios.php';})</script>";
+        }
+    }
+
+
+    if (isset($mensagem)) { // verifica se há alguma mensagem de erro
+        echo "<script type='text/javascript'>Swal.fire('Ops!', '$mensagem', 'error')</script>"; // exibe a mensagem de erro
+    }
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Cadastro Aluno</title>
+    <link rel="stylesheet" href="../assets/css/Cadastro.css">
+</head>
+
+<body>
+
+    <div class="container">
+        <div class="form-image">
+            <img src="../assets/img/Cadastro_cliente.svg" width="" height="">
+        </div>
+        <div class="form">
+            <form action="C_usuario.php" method="POST">
+                <div class="form-register">
+                    <div class="title">
+                        <h1>ADICIONAR CLIENTE</h1>
+                    </div>
+                    <div class="voltar-button">
+                        <button><a href="../SISTEMA/Usuarios.php">Voltar</a></button>
+                    </div>
+                </div>
+                <div class="input-group">
+                    <div class="input-box">
+                        <label for="nome">Nome</label>
+                        <input id="nome" type="text" name="nome" placeholder="Digite seu nome" required>
+                    </div>
+
+                    <div class="input-box">
+                        <label for="username">Cidade</label>
+                        <input id="cidade" type="text" name="cidade" placeholder="Digite sua cidade " required>
+                    </div>
+
+
+                    <div class="input-box">
+                        <label for="endereço">Endereço</label>
+                        <input id="endereço" type="text" name="endereço" placeholder="Digite seu Endereço" required>
+                    </div>
+
+                    <div class="input-box">
+                        <label for="email">E-mail</label>
+                        <input id="email" type="email" name="email" placeholder="Digite seu e-mail" required>
+                    </div>
+                </div>
+
+                <div class="continue-button">
+                    <input name="submit" type="submit" value="Cadastrar">
+                </div>
+            </form>
+        </div>
+    </div>
+</body>
+
+</html>
---
+++ WDA-Livraria(Web)/public_html/WDA-Livraria/SISTEMA/Usuarios.php
@@ -0,0 +1,174 @@
+<?php
+// Inicia a sessão
+session_start();
+
+// Inclui o arquivo de configuração da conexão com o banco de dados
+include_once("../Config.php");
+
+// Verifica se há uma busca na URL
+if (!empty($_GET['search'])) {
+    $data = $_GET['search'];
+    // Monta a consulta SQL com o critério de busca
+    $sql = "SELECT * FROM usuario WHERE Cod_usuario LIKE '%$data%' or Nome_usuario LIKE '%$data%' or Email LIKE '%$data%' ORDER BY Cod_usuario DESC";
+} else {
+    // Monta a consulta SQL sem o critério de busca
+    $sql = "SELECT * FROM usuario ORDER BY Cod_usuario DESC";
+    
+}
+
+// Executa a consulta SQL
+$result = $conexao->query($sql);
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="loading-bar.css"/>
+    <script type="text/javascript" src="loading-bar.js"></script>
+    <link rel="stylesheet" href="../assets/css/Tabela.css">
+    <title>CLIENTES</title>
+</head>
+
+<body>
+    <!-- Código HTML para exibição da barra de navegação e da tabela de clientes -->
+    <!-- Barra de navegação -->
+    <nav class="navbar navbar-expand-lg" style="background-color: rgb(255,255,255);">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="Sistema.php">WDA LIVRARIA</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarText">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <!-- Links para as diferentes páginas -->
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="Sistema.php">INICIO
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="Usuarios.php">CLIENTES</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="Livros.php">LIVROS</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="Editoras.php">EDITORAS</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="Aluguel.php">ALUGUEL</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="Historico.php">HISTÓRICO</a>
+                    </li>
+                </ul>
+                <!-- Botão de sair -->
+                <span class="navbar-text">
+                    <div class="d-flex">
+                        <a href="Sair.php" id="out" class="btn_out btn btn-outline-danger me-5 ">Sair</a>
+                    </div>
+                </span>
+            </div>
+        </div>
+    </nav>
+    <br><br>
+
+    <!-- Tabela de clientes -->
+    <div class="table-responsive m-5">
+        <div class="header-table d-flex justify-content-between align-items-center">
+            <h2>Clientes</h2>
+            <!-- Botão para adicionar novo cliente -->
+            <a href="../CREATE/C_usuario.php" class="btn btn-success" style="margin: 3px 3px 3px 3px;">ADICIONAR +</a><br>
+            <!-- Campo de busca -->
+            <input type="search" class="form-control w-25" placeholder="Pesquisar" id="pesquisar">
+            <!-- Botão para executar a busca -->
+            <button class="btn btn-primary" onclick="searchData()">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+                    <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
+                </svg>
+            </button>
+        </div>
+        <!-- Tabela propriamente dita -->
+        <table class="table text-white table-bg">
+            <thead class="table-header">
+                <tr>
+                    <th scope="col">ID</th>
+                    <th scope="col">Nome</th>
+                    <th scope="col">Cidade</th>
+                    <th scope="col">Endereço</th>
+                    <th scope="col">Email</th>
+                    <th scope="col">Ações</th>
+                </tr>
+            </thead>
+
+            <body>
+                <?php
+                // Laço while para percorrer todos os dados de usuário obtidos do banco de dados
+                while ($user_data = mysqli_fetch_assoc($result)) {
+                    // Imprime uma linha da tabela HTML, com as informações de um usuário
+                    echo "<tr>";
+                    echo "<td>" . $user_data['Cod_usuario'] . "</td>";
+                    echo "<td>" . $user_data['Nome_usuario'] . "</td>";
+                    echo "<td>" . $user_data['Cidade'] . "</td>";
+                    echo "<td>" . $user_data['Endereço'] . "</td>";
+                    echo "<td>" . $user_data['Email'] . "</td>";
+                    // Imprime os botões de edição e deleção para cada usuário na linha da tabela HTML
+                    echo "<td>
+        <a class='btn btn-sm btn-primary' href='../UPDATE/U_usuario.php?id=$user_data[Cod_usuario]' title='Editar'>
+        <svg xmlns='http://www.w3.org/2000/svg' width='20' height='20' fill='currentColor' class='bi bi-pencil' viewBox='0 0 16 16'>
+        <path d='M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z'/>
+            </svg>
+            </a> 
+        <a class='btn btn-sm btn-danger' href='#' title='Deletar' onclick='confirmDelete($user_data[Cod_usuario])'>
+        <svg xmlns='http://www.w3.org/2000/svg' width='20' height='20' fill='currentColor' class='bi bi-trash-fill' viewBox='0 0 16 16'>
+        <path d='M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z'/>
+        </svg>
+        </a>
+        </td>";
+     echo "</tr>";
+                }
+                ?>
+            </body>
+        </table>
+    </div>
+</body>
+<script>
+    var search = document.getElementById('pesquisar')
+
+    search.addEventListener("keydown", function(event) {
+        if (event.key === "Enter") {
+            searchData()
+        }
+    })
+
+    function searchData() {
+        window.location = 'Usuarios.php?search=' + search.value
+    }
+</script>
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.7/dist/umd/popper.min.js" integrity="sha384-zYPOMqeu1DAVkHiLqWBUTcbYfZ8osu1Nd6Z89ify25QV9guujx43ITvfi12/QExE" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.min.js" integrity="sha384-Y4oOpwW3duJdCWv5ly8SCFYWqFDsfob/3GkgExXKV4idmbt98QcxXYs9UoXAB7BZ" crossorigin="anonymous"></script>
+<script>
+function confirmDelete(id) {
+    Swal.fire({
+        title: 'Deletar cliente e histórico?',
+        text: "Você não poderá reverter isso!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Sim, exclua!',
+        cancelButtonText: 'Cancelar'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            window.location.href = `../DELETE/D_usuario.php?id=${id}`;
+        }
+    });
+}
+</script>
+
+</html>
